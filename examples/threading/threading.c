0903943288bbd37ee1f674236a4c2d1f65e66ea5
gavrilenkoof <vanche123@gmail.com>
2026-05-24 12:05:10 +0200

    implemented assignment-4-part-1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -1,4 +1,5 @@
 #include "threading.h"
+#include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -14,6 +15,30 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    int rc = 0;
+
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+
+    rc = pthread_mutex_lock(thread_func_args->mutex);
+    if(rc != 0)
+    {
+        ERROR_LOG("Failed to mutex lock, error was %d\r\n", rc);
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+    rc = pthread_mutex_unlock(thread_func_args->mutex);
+    if(rc != 0)
+    {
+        ERROR_LOG("Failed to mutex unlock, error was %d\r\n", rc);
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+
+    thread_func_args->thread_complete_success = true;
+
     return thread_param;
 }
 
@@ -28,6 +53,51 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+    struct thread_data *data = (struct thread_data *)malloc(sizeof(struct thread_data));
+    if(!data)
+    {
+        return false;
+    }
+
+    int rc = 0;
+
+    // rc = pthread_mutex_init(mutex, NULL);
+    // if(rc != 0)
+    // {
+        // ERROR_LOG("Failed to init mutex, error was %d\r\n", rc);
+        // free(data);
+        // return false;
+    // }
+
+    data->mutex = mutex;
+    data->wait_to_obtain_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+    data->thread_complete_success = false;
+
+    rc = pthread_create(thread, NULL, threadfunc, (void *)data);
+    if(rc != 0)
+    {
+        ERROR_LOG("Failed to thread create, error was %d\r\n", rc);
+        // pthread_mutex_destroy(data->mutex);
+        free(data);
+        return false;
+    }
+    
+    
+    // rc = pthread_join(*thread, (void *)data);
+    // if(rc != 0)
+    // {
+    //     ERROR_LOG("Failed to thread join, error was %d\r\n", rc);
+    //     // pthread_mutex_destroy(data->mutex);
+    //     free(data);
+    //     return false;
+    // }
+
+    // bool thread_ret = data->thread_complete_success;
+    // free(data);
+
+    // return thread_ret;
+    return true;
 }
 
